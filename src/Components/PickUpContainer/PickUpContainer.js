cc117e48f7a17be7f1f013b03670415f851df010
github username <github email address>
2019-06-04 10:23:07 -0600

    hiding api key

--- src/Components/PickUpContainer/PickUpContainer.js
+++ src/Components/PickUpContainer/PickUpContainer.js
@@ -4,10 +4,10 @@ import ApiConfig from '../../google_api'
 const PickUpContainer = (props) => {
 
   const {loggedIn} = props
-
+  const API_KEY = `${process.env.REACT_APP_MAPS_API_KEY}`
   return(
     <div>
-      {loggedIn ? <p>{ApiConfig.googleAPI}</p> : <p>You must be logged in to access this portion of the site.</p>}
+      {loggedIn ? <p>{API_KEY}</p> : <p>You must be logged in to access this portion of the site.</p>}
     </div>
   )
 }
--- src/google_api.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  googleAPI: "AIzaSyAVlFp4Kvo4tVL4_-cwFJXCEx8mzc9EGf4"
-}
