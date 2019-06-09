a66767a36b5b22160fb0b086bb1fe94193be3cc4
Mark Pearyer <marcusp619@gmail.com>
2019-06-09 11:03:37 -0600

    Add a icon hash with comments

--- src/Components/PickUpEvent/PickUpEvent.js
+++ src/Components/PickUpEvent/PickUpEvent.js
@@ -3,15 +3,23 @@ import './PickUpEvent.css'
 import spikeball from '../../markers/spikeball.png'
 import soccer from '../../markers/soccer.png'
 
+// can be a functional component
 class PickUpEvent extends Component {
 
   toggleDetailsShow = () => {
-    const {toggleShowEventDetails, event} = this.props
+    const { toggleShowEventDetails, event } = this.props
     toggleShowEventDetails(event)
   }
 
+  iconHash = {
+    "Spikeball": spikeball,
+    "Soccer": soccer,
+  }
 
-  render () {
+  render() {
+    // could be a hash table for better perfomance
+    // this at best is O(1) at most cases its O(n)
+    // a hash is always constant O(1)
     const icon = () => {
       switch (this.props.event.sport) {
         case "Spikeball":
@@ -26,8 +34,10 @@ class PickUpEvent extends Component {
     return (
       <div className="marker" onClick={this.toggleDetailsShow}>
         <img alt="event location" src={icon()} />
+        <img alt="event location" src={this.iconHash[this.props.event.sport] ? this.iconHash[this.props.event.sport] : soccer} />
       </div>
     )
   }
 }
+
 export default PickUpEvent
