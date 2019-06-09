4feae1a4d1f292bd0966b5a07c205803015961fd
Mark Pearyer <marcusp619@gmail.com>
2019-06-09 10:30:47 -0600

    Removes extra div thats not needed in your HTML.

--- src/App.js
+++ src/App.js
@@ -1,94 +1,129 @@
-import React, {Component} from 'react'
-import './App.css'
-import {BrowserRouter as Router, Route, NavLink} from 'react-router-dom';
-import UserLogin from './Components/UserLogin/UserLogin'
-import NewUserForm from './Components/NewUserForm/NewUserForm'
-import HomePage from './Components/HomePage/HomePage'
-import PickUpContainer from './Components/PickUpContainer/PickUpContainer'
-import NewPickUpForm from './Components/NewPickUpForm/NewPickUpForm'
+import React, { Component, Fragment } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
+import UserLogin from "./Components/UserLogin/UserLogin";
+import NewUserForm from "./Components/NewUserForm/NewUserForm";
+import HomePage from "./Components/HomePage/HomePage";
+import PickUpContainer from "./Components/PickUpContainer/PickUpContainer";
+import NewPickUpForm from "./Components/NewPickUpForm/NewPickUpForm";
 
 class App extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
       loggedIn: false,
       showLogIn: false,
       showNewUserForm: false,
       userId: ""
-    }
+    };
   }
 
   componentDidMount = () => {
-    if (localStorage.getItem('pickUpLogin')) {
-      this.logIn(localStorage.getItem('pickUpUser'), localStorage.getItem('pickUpLogin'))
+    if (localStorage.getItem("pickUpLogin")) {
+      this.logIn(
+        localStorage.getItem("pickUpUser"),
+        localStorage.getItem("pickUpLogin")
+      );
     }
-  }
+  };
 
   logIn = (userId, token) => {
-    localStorage.setItem('pickUpUser', userId)
-    localStorage.setItem('pickUpLogin', token)
-    this.setState({userId, showLogIn: false, loggedIn: true})
-  }
+    localStorage.setItem("pickUpUser", userId);
+    localStorage.setItem("pickUpLogin", token);
+    this.setState({ userId, showLogIn: false, loggedIn: true });
+  };
 
   logOut = () => {
-    localStorage.clear()
-    window.location.href = "http://localhost:3001/"
-    this.setState({loggedIn: false, userId: "", showLogIn: false})
-  }
+    localStorage.clear();
+    window.location.href = "http://localhost:3001/";
+    this.setState({ loggedIn: false, userId: "", showLogIn: false });
+  };
 
   toggleShowLogIn = () => {
     this.setState({
       showLogIn: !this.state.showLogIn
-    })
-  }
+    });
+  };
 
   toggleShowNewUserForm = () => {
     this.setState({
       showNewUserForm: !this.state.showNewUserForm
-    })
-  }
+    });
+  };
 
   render() {
-    const {loggedIn, showLogIn, showNewUserForm} = this.state
+    const { loggedIn, showLogIn, showNewUserForm } = this.state;
 
     return (
-      <div>
+      <Fragment>
         <header>
           <h1 className="app-name">Pick Up Sports!</h1>
-          { loggedIn
-            ? <button className="button log-out user-credentials" onClick={this.logOut}>Log Out</button>
-            : <div className="user-credentials">
-                <button onClick={this.toggleShowLogIn}>Log In</button>
-                <button onClick={this.toggleShowNewUserForm} className="create-act button">Create Account</button>
-                <NewUserForm showNewUserForm={showNewUserForm} toggleShowNewUserForm={this.toggleShowNewUserForm} logIn={this.logIn}/>
-              </div>}
-          { showLogIn ? <UserLogin logIn={this.logIn}/> : null}
+          {loggedIn ? (
+            <button
+              className="button log-out user-credentials"
+              onClick={this.logOut}
+            >
+              Log Out
+            </button>
+          ) : (
+            <div className="user-credentials">
+              <button onClick={this.toggleShowLogIn}>Log In</button>
+              <button
+                onClick={this.toggleShowNewUserForm}
+                className="create-act button"
+              >
+                Create Account
+              </button>
+              <NewUserForm
+                showNewUserForm={showNewUserForm}
+                toggleShowNewUserForm={this.toggleShowNewUserForm}
+                logIn={this.logIn}
+              />
+            </div>
+          )}
+          {showLogIn ? <UserLogin logIn={this.logIn} /> : null}
         </header>
         <Router>
           <div>
             <nav>
               <ul>
                 <li>
-                  <NavLink exact={true} to="/">Home</NavLink>
+                  <NavLink exact={true} to="/">
+                    Home
+                  </NavLink>
                 </li>
                 <li>
-                  <NavLink exact={true} to="/pickups">All Pick Up Games</NavLink>
+                  <NavLink exact={true} to="/pickups">
+                    All Pick Up Games
+                  </NavLink>
                 </li>
                 <li>
-                  <NavLink exact={true} to="/addpickup">Create a New Pick Up Game</NavLink>
+                  <NavLink exact={true} to="/addpickup">
+                    Create a New Pick Up Game
+                  </NavLink>
                 </li>
               </ul>
             </nav>
             <div className="containers">
-              <Route exact={true} path="/" component={() => <HomePage loggedIn={loggedIn}/>} />
-              <Route path="/pickups" component={() => <PickUpContainer loggedIn={loggedIn}/>} />
-              <Route path="/addpickup" component={() => <NewPickUpForm loggedIn={loggedIn}/>} />
+              <Route
+                exact={true}
+                path="/"
+                component={() => <HomePage loggedIn={loggedIn} />}
+              />
+              <Route
+                path="/pickups"
+                component={() => <PickUpContainer loggedIn={loggedIn} />}
+              />
+              <Route
+                path="/addpickup"
+                component={() => <NewPickUpForm loggedIn={loggedIn} />}
+              />
             </div>
           </div>
         </Router>
-    </div>
-  )
-}
+      </Fragment>
+    );
+  }
 }
 
-export default App
+export default App;
